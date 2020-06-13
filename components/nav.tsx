3c15dbee0150ced098744c0b5807f683c5dfd27b
Tarrazu <yuyaebean@gmail.com>
2020-06-13 17:45:30 +0900

    [Nav] Change nav color

--- components/nav.tsx
+++ components/nav.tsx
@@ -8,16 +8,16 @@ export default function Nav() {
       <ul className='flex justify-between items-center p-8 md:px-32'>
         <li>
           <Link href='/'>
-            <a className='text-blue-500 no-underline mr-8'>Home</a>
+            <a className='text-teal-500 no-underline mr-8'>Home</a>
           </Link>
           <Link href='/profile'>
-            <a className='text-blue-500 no-underline'>Profile</a>
+            <a className='text-teal-500 no-underline'>Profile</a>
           </Link>
         </li>
         <ul className='flex justify-between items-center space-x-4'>
           {links.map(({ href, label }) => (
             <li key={label}>
-              <a href={href} className='btn-blue no-underline' target='_blank'>
+              <a href={href} className='btn-teal no-underline' target='_blank'>
                 {label}
               </a>
             </li>
